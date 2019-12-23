348501a873038e62d4c83d4283ffd10aa18b5f09
dwags618 <dwags618@gmail.com>
2019-12-22 21:34:35 -0800

    campground destroy

--- v10/routes/campgrounds.js
+++ v10/routes/campgrounds.js
@@ -81,6 +81,17 @@ router.put("/:id", function(req, res){
 	})
 })
 
+//destroy campground route
+router.delete("/:id", function(req, res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds")
+		} else{
+			res.redirect("/campgrounds")
+		}
+	})
+})
+
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next()
